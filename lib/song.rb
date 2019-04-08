f39ca0ba64ad8d013c4ef13d17351f6cf6ddac39
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-08 20:48:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,11 @@ class Song
     end
 
     def artist_name
-      self.artist.name
+      if self.artist
+         self.artist.name
+       else
+         nil
+       end
     end
 
     def self.all

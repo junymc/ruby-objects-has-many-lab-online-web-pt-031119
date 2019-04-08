7bf08acc69b86e2655bb763b11830eea61a1a134
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-08 20:44:36 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,7 @@
 class Author
 
   attr_accessor :name
+  @@post_count = 0
 
     def initialize(name)
       @name = name
@@ -10,6 +11,7 @@ class Author
     def add_post(post)
       @posts << post
       post.author = self
+      @@post_count += 1
     end
 
     def posts
